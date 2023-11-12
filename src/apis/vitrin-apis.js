2fa0309d670cfe3edd522b96bd9bd9ea29d25a57
MFeiz <30654631+mohammadFeiz@users.noreply.github.com>
2023-11-12 14:32:54 +0330

    Update vitrin-apis.js

--- src/apis/vitrin-apis.js
+++ src/apis/vitrin-apis.js
@@ -37,7 +37,7 @@ export default function vitrinApis({baseUrl,helper}) {
             return {result:ids}
         },
         async v_getProductsByIds(ids,{apis}){
-            let products = await apis.request({api:'kharid.getSpreeProducts',parameter:{ ids }});
+            let products = await apis.request({api:'kharid.getSpreeProducts',parameter:{ ids,Taxons:'10673' }});
             products = await apis.request({api:'kharid.updateProductPrice',parameter:{ products, cartId: 'Regular' }});
             products = products.map((o)=>{return {id : o.id , name : o.name , price:o.FinalPrice , src:o.srcs[0] , inStock:true , code : o.defaultVariant?o.defaultVariant.code:o.code}  });
             return {result:products}
